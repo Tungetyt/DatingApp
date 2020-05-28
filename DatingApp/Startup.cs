28f4c40f2dcaa952aae1c185c10f8dd2352ebb3f
MK <m0kotlicki@gmail.com>
2020-05-28 16:18:57 +0200

    Added Service and IService class

--- DatingApp/Startup.cs
+++ DatingApp/Startup.cs
@@ -26,6 +26,7 @@ namespace DatingApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddScoped<IService, Service>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
